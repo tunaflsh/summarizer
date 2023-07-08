c5b508ece0b192ccac70df46d957c2c8dd15e249
tunaflsh <bumtuan2001@gmail.com>
2023-07-08 21:15:53 +0200

    Add prompt.py
    Contains prompts to guide and instruct the LLMs.

---
+++ prompt.py
@@ -0,0 +1,11 @@
+PHASE1 = '''
+Take Notes from the Text provided by the user on the topic "{topic}" in {language}. It should be structured using markdown and be densely packed with information so that the original text can be reconstructed from it.
+'''
+
+PHASE2A = '''
+Reconstruct the original text from the Notes provided by the user on the topic "{topic}" in {language}.
+'''
+
+PHASE2B = '''
+Create a detailed technical Report in {language} on the topic "{topic}" using the information gathered in the Notes provided by the user.
+'''
